2231eaa40c6c02e5a8f8b0a533f6c2247594847e
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2023-01-26 21:36:26 -0700

    Add info to array

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 const URL = "https://www.flipkart.com/search?q=mobiles";
+const arr = [];
 
 request(URL, (err, res, body) => {
   if (err) {
@@ -12,8 +13,13 @@ request(URL, (err, res, body) => {
     $("div._1AtVbE > div._13oc-S").each(function (index) {
       const link = $(this).find("div._2kHMtA>a").attr("href");
       const name = $(this).find("div._3pLy-c>div.col-7-12>div._4rR01T").text();
-      console.log(link); //link for smartphone
-      console.log(name); //name of smartphone
+      const obj = {
+        link: link,
+        name: name,
+      };
+      //console.log(obj);
+      arr.push(JSON.stringify(obj));
     });
   }
+  console.log(arr);
 });
